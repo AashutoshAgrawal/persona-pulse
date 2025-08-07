679f7405f2d2eb7758f4a2642f396a1cb3c04d3b
AashutoshAgrawal <33202172+AashutoshAgrawal@users.noreply.github.com>
2025-08-07 11:09:37 -0400

    fixing build steps

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+const basePath = process.env.VITE_BASE_PATH || "/";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: basePath,
   server: {
     host: "::",
     port: 8080,
