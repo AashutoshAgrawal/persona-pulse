e623b03a18ceacdc31ac9f237c0b4cb9552a01cf
AashutoshAgrawal <33202172+AashutoshAgrawal@users.noreply.github.com>
2025-08-06 23:28:31 -0400

    added github actions

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "https://github.com/AashutoshAgrawal/persona-pulse",
   plugins: [
     react(),
     mode === 'development' &&
